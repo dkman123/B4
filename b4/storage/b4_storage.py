8d366d29f403de7c20c9274879d893fe8c37b079
dkman123 <dkman123@hotmail.com>
2024-02-19 18:54:07 -0500

    experimenting

--- b4/storage/b4_storage.py
+++ b4/storage/b4_storage.py
@@ -18,137 +18,136 @@
 
 import datetime
 # DO NOT REMOVE
+import b4
 #import b4.storage.b4_mysql
 
 __author__ = 'Courgette'
 __version__ = '1.2'
 
-import b4.storage.b4_mysql
-
 PROTOCOLS = ('mysql', 'sqlite', 'postgresql')
 
-
-class mapresult(object):
-    # initialization function
-    def __init__(self, mapname, redscore, bluescore, maptime, lowplayer, highplayer, createddate, id):
-        if id:
-            self.id = id
-        else:
-            self.id = 0
-            # self.id = None
-        self.mapname = mapname
-        self.redscore = redscore
-        self.bluescore = bluescore
-        self.maptime = maptime
-        self.lowplayer = lowplayer
-        self.highplayer = highplayer
-        if createddate:
-            self.createddate = createddate
-        else:
-            self.createddate = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-            # self.createddate = None
-
-
-# started the process of having it save abuse targets, but never finished it
-# class abuseresult(object):
+#
+# class mapresult(object):
 #     # initialization function
-#     def __init__(self, client_id, abusetype, createddate, id):
+#     def __init__(self, mapname, redscore, bluescore, maptime, lowplayer, highplayer, createddate, id):
 #         if id:
 #             self.id = id
 #         else:
 #             self.id = 0
 #             # self.id = None
-#         self.client_id = client_id
-#         self.abusetype = abusetype
+#         self.mapname = mapname
+#         self.redscore = redscore
+#         self.bluescore = bluescore
+#         self.maptime = maptime
+#         self.lowplayer = lowplayer
+#         self.highplayer = highplayer
 #         if createddate:
 #             self.createddate = createddate
 #         else:
 #             self.createddate = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
 #             # self.createddate = None
-
-
-class Storage(object):
-
-    console = None
-    protocol = None
-
-    def connect(self):
-        raise NotImplementedError
-
-    def shutdown(self):
-        raise NotImplementedError
-
-    def getConnection(self):
-        raise NotImplementedError
-
-    def getCounts(self):
-        raise NotImplementedError
-    
-    def getClient(self, client):
-        raise NotImplementedError
-    
-    def getClientsMatching(self, match):
-        raise NotImplementedError
-    
-    def setClient(self, client):
-        raise NotImplementedError
-    
-    def setClientAlias(self, alias):
-        raise NotImplementedError
-    
-    def getClientAlias(self, alias):
-        raise NotImplementedError
-    
-    def getClientAliases(self, client):
-        raise NotImplementedError
-    
-    def setClientIpAddress(self, ipalias):
-        raise NotImplementedError
-    
-    def getClientIpAddress(self, ipalias):
-        raise NotImplementedError
-    
-    def getClientIpAddresses(self, client):
-        raise NotImplementedError
-    
-    def getLastPenalties(self, types='Ban', num=5):
-        raise NotImplementedError
-
-    def setClientPenalty(self, penalty):
-        raise NotImplementedError
-    
-    def getClientPenalty(self, penalty):
-        raise NotImplementedError
-    
-    def getClientPenalties(self, client, penType='Ban'):
-        raise NotImplementedError
-    
-    def getClientLastPenalty(self, client, penType='Ban'):
-        raise NotImplementedError
-    
-    def getClientFirstPenalty(self, client, penType='Ban'):
-        raise NotImplementedError
-    
-    def disableClientPenalties(self, client, penType='Ban'):
-        raise NotImplementedError
-    
-    def numPenalties(self, client, penType='Ban'):
-        raise NotImplementedError
-    
-    def getGroups(self):
-        raise NotImplementedError
-
-    def getGroup(self, group):
-        raise NotImplementedError
-
-    def getTables(self):
-        raise NotImplementedError
-
-    def truncateTable(self, table):
-        raise NotImplementedError
-
-    def status(self):
-        raise NotImplementedError
+#
+#
+# # started the process of having it save abuse targets, but never finished it
+# # class abuseresult(object):
+# #     # initialization function
+# #     def __init__(self, client_id, abusetype, createddate, id):
+# #         if id:
+# #             self.id = id
+# #         else:
+# #             self.id = 0
+# #             # self.id = None
+# #         self.client_id = client_id
+# #         self.abusetype = abusetype
+# #         if createddate:
+# #             self.createddate = createddate
+# #         else:
+# #             self.createddate = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+# #             # self.createddate = None
+#
+#
+# class Storage(object):
+#
+#     console = None
+#     protocol = None
+#
+#     def connect(self):
+#         raise NotImplementedError
+#
+#     def shutdown(self):
+#         raise NotImplementedError
+#
+#     def getConnection(self):
+#         raise NotImplementedError
+#
+#     def getCounts(self):
+#         raise NotImplementedError
+#
+#     def getClient(self, client):
+#         raise NotImplementedError
+#
+#     def getClientsMatching(self, match):
+#         raise NotImplementedError
+#
+#     def setClient(self, client):
+#         raise NotImplementedError
+#
+#     def setClientAlias(self, alias):
+#         raise NotImplementedError
+#
+#     def getClientAlias(self, alias):
+#         raise NotImplementedError
+#
+#     def getClientAliases(self, client):
+#         raise NotImplementedError
+#
+#     def setClientIpAddress(self, ipalias):
+#         raise NotImplementedError
+#
+#     def getClientIpAddress(self, ipalias):
+#         raise NotImplementedError
+#
+#     def getClientIpAddresses(self, client):
+#         raise NotImplementedError
+#
+#     def getLastPenalties(self, types='Ban', num=5):
+#         raise NotImplementedError
+#
+#     def setClientPenalty(self, penalty):
+#         raise NotImplementedError
+#
+#     def getClientPenalty(self, penalty):
+#         raise NotImplementedError
+#
+#     def getClientPenalties(self, client, penType='Ban'):
+#         raise NotImplementedError
+#
+#     def getClientLastPenalty(self, client, penType='Ban'):
+#         raise NotImplementedError
+#
+#     def getClientFirstPenalty(self, client, penType='Ban'):
+#         raise NotImplementedError
+#
+#     def disableClientPenalties(self, client, penType='Ban'):
+#         raise NotImplementedError
+#
+#     def numPenalties(self, client, penType='Ban'):
+#         raise NotImplementedError
+#
+#     def getGroups(self):
+#         raise NotImplementedError
+#
+#     def getGroup(self, group):
+#         raise NotImplementedError
+#
+#     def getTables(self):
+#         raise NotImplementedError
+#
+#     def truncateTable(self, table):
+#         raise NotImplementedError
+#
+#     def status(self):
+#         raise NotImplementedError
 
 
 #from b4_mysql import MysqlStorage
